77c21baf3150bc8b27bf37ad280c920ed60e4e8e
Bleum <30318917+Catnap421@users.noreply.github.com>
2021-04-17 01:43:13 +0900

    Update reorder-data-in-log-files.py

--- reorder-data-in-log-files/reorder-data-in-log-files.py
+++ reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -11,4 +11,5 @@ class Solution:
         return sorted_logs + digits
         
         # 이러한 정렬 문제에 특히나 약점을 보인다! 복습하자!
-        
+        # 정렬에서 동일한 값일 경우, 후순위 식별자를 아래와 같이 설정할 수 있다!! 명심해두자
+        # sorted_logs = sorted(letters, key=lambda x: (x.split()[1:], x.split()[0]))
