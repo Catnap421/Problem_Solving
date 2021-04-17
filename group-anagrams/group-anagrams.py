fe6fed3f02f88d5a61e46bb32d29f55eb6915047
Bleum <30318917+Catnap421@users.noreply.github.com>
2021-04-17 11:17:30 +0900

    Update group-anagrams.py
    Add: 애너그램의 정렬한 단어를 키로 한 풀이법

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -24,6 +24,15 @@ class Solution:
     """
     이렇게 temp를 이용해 이전 배열의 원소와 비교해 return하는 경우는 꼭 마지막 경우를 생각하는 습관을 들이자!
     (like 19 lines)
+    
+    # 다른 풀이
+    애너그램의 문제의 경우, 정렬한 단어를 key로 하는 딕셔너리를 만들어 해결이 가능!
+    
+    anagrams = collections.defaultdict(list)
+    
+    for word in strs:
+        anagrams[''.join(sorted(word))].append(word)
+    return list(anagrams.values())
 
     """
             
