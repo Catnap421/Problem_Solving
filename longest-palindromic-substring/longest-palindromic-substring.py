a1643643de469cd44fde52ab642cf5a182e29f9c
Bleum <30318917+Catnap421@users.noreply.github.com>
2021-04-18 01:30:53 +0900

    Update longest-palindromic-substring.py
    Add: 책 풀이(max - key 설정 가능, 다양한 관점을 동시에 접근하기)

--- longest-palindromic-substring/longest-palindromic-substring.py
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -12,4 +12,32 @@ class Solution:
     """
     알고리즘 접근은 좋았지만, 하나를 놓쳐서 계속 시도한 케이스. 끝까지 변수가 어떤 값을 가지는 지 잘 확인하자!
     """
+ 
+
+# 책 풀이    
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(left:int, right:int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1: right]
+        
+        if len(s) < 2 or s == s[::-1]:
+            return s
+        
+        result = ''
+        for i in range(len(s) - 1):
+            result = max(result,
+                         expand(i, i + 1),
+                         expand(i, i + 2),
+                         key = len)
+        
+        return result
+    
+    """
+    1. max에도 key를 설정할 수 있다.
+    2. 투 포인터를 설정해, 문제를 접근할 수 있다. (다양한 관점을 동시에 접근)
+    """
+    
         
